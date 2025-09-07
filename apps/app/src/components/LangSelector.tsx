d54f8023f17c1ae215d0f840eed89e270709e891
Billy Le <hi@billyle.dev>
2025-09-07 13:45:27 -0500

    Add UserProfileForm

--- apps/app/src/components/LangSelector.tsx
+++ apps/app/src/components/LangSelector.tsx
@@ -1,30 +1,49 @@
-import { Id } from '@ahpatuh/convex/_generated/dataModel';
-
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from '@ahpatuh/ui/components/select';
+import type { Id } from '@ahpatuh/convex/_generated/dataModel';
 type Lang = { _id: Id<'languages'>; value: string; name: string };
 
 interface LangSelectorProps {
   langs: Lang[];
   onLangChange: (lang: Lang) => void;
   defaultLang?: Lang | null;
+  disabled?: boolean;
 }
 
 export const LangSelector = ({
   langs,
   onLangChange,
   defaultLang,
+  disabled,
 }: LangSelectorProps) => {
   return (
-    <select
-      defaultValue={defaultLang?.value ?? 'en-US'}
-      onChange={(e) => {
-        onLangChange(langs.find((lang) => e.target.value === lang.value)!);
+    <Select
+      onValueChange={(value) => {
+        onLangChange(langs.find((lang) => lang.value === value)!);
       }}
+      defaultValue={defaultLang?.value ?? 'en-US'}
+      disabled={disabled}
     >
-      {langs.map((lang) => (
-        <option key={lang.value} value={lang.value}>
-          {lang.name}
-        </option>
-      ))}
-    </select>
+      <SelectTrigger className='border border-black'>
+        <SelectValue placeholder='Select a language' />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Languages</SelectLabel>
+          {langs.map((lang) => (
+            <SelectItem key={lang.value} value={lang.value}>
+              {lang.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
